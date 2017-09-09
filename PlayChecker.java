c65dd4d9c580260c3c8c6d99345a38733ac3590a
Alexander <icsd15029@icsd.aegean.gr>
2017-09-09 18:14:38 +0300

    Delete PlayChecker.java

--- PlayChecker.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class PlayChecker
-{
-    public static void main(String[] args) throws IOException
-    {
-        new SyntacticAnalysis(args[0]).parse();
-    }
-}
